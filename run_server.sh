ab56d8d462d392167eeff5e4b157250fde09e2dc
Jonne <jonne.k.saleva@pm.me>
2018-07-25 04:59:36 +0000

    modified port & reqs

--- run_server.sh
+++ run_server.sh
@@ -3,4 +3,4 @@
 #python src/endpoint.py --corpus_path='./data/enron_corpora_final.pkl'
 pkill gunicorn
 #git pull
-gunicorn -b 0.0.0.0:8000 wsgi:app --reload
+gunicorn -b 0.0.0.0:80 wsgi:app --reload
