fd8038d89a8304ac4f60004e11b8f620cb863f4c
JKS <jonne@jonnesaleva.com>
2019-04-20 11:57:16 -0400

    edit run_server.sh

--- run_server.sh
+++ run_server.sh
@@ -1,5 +1,3 @@
-# run_service.sh
-#python src/endpoint.py --corpus_path='./data/enron_corpora_final.pkl'
+# run_server.sh
 sudo pkill gunicorn
-#git pull
 gunicorn -b 127.0.0.1:8000  wsgi:app --reload
