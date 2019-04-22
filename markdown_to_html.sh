63accb0d593939bcf65f4876dbedf51014d7ef99
JKS <jonne@jonnesaleva.com>
2019-04-21 22:23:27 -0400

    add comments to markdown_to_html.sh

--- markdown_to_html.sh
+++ markdown_to_html.sh
@@ -1,11 +1,23 @@
+# iterate over .md files in /src/static/md
 for f in ./src/static/md/*.md
 do
+  # grab stub, i.e. name of file without path or extension
   STUB=$(echo $f | \
          sed s/".\/src\/static\/md\/"/''/g | \
          sed s/".md"/''/g);
+
+  # compose path to output HTML file using the stub
   OUT_PATH=./src/templates/$STUB.html
+
+  # convert the .md file to html and store the body in a variable
   PAGE_BODY=$(pandoc -t html $f);
+
+  # remove old version of output HTML file
   rm $OUT_PATH;
+
+  ## CREATING NEW HTML FILE ##
+
+  # append header of HTML page to output file
   echo \
 """
 <html xmlns="http://www.w3.org/1999/xhtml">
@@ -15,8 +27,12 @@ do
   <link rel="stylesheet" href="../static/css/style.css" type="text/css" />
 </head>
 """ >> $OUT_PATH;
+
+  # create body of output HTML document and append it to output file
   echo "<body>" >> $OUT_PATH;
   echo $PAGE_BODY >> $OUT_PATH;
+
+  # finally close out the <body> and <html> tags
   echo \
 """
 </body>
