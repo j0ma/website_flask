e95510da1a14ab18cba49a8233e973f810a96354
JKS <jonne@jonnesaleva.com>
2019-05-14 00:50:53 -0400

    rm checks

--- run_server.sh
+++ run_server.sh
@@ -1,10 +1,7 @@
 # run_server.sh
-run_flask_server () {
-  sudo pkill gunicorn
-  gunicorn -b 127.0.0.1:8000 wsgi:app \
-           --reload \
-           --certfile $CERTBOT_CERTFILE \
-           --keyfile $CERTBOT_KEYFILE
-}
+xsudo pkill gunicorn
+gunicorn -b 127.0.0.1:8000 wsgi:app \
+         --reload \
+         --certfile ./fullcert.pem \
+         --keyfile ./privkey.pem
 
-./check_env_vars.sh || run_flask_server
