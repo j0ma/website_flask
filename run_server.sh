64b43f71345815063e4d2e90f6878764c6e18f9e
JKS <jonne@jonnesaleva.com>
2019-05-15 20:42:13 -0400

    temp disable keys

--- run_server.sh
+++ run_server.sh
@@ -1,7 +1,7 @@
 # run_server.sh
 sudo pkill gunicorn
 gunicorn -b 127.0.0.1:8000 wsgi:app \
-         --reload \
-         --certfile ./fullchain.pem \
-         --keyfile ./privkey.pem
+         --reload #\
+#         --certfile ./fullchain.pem \
+#         --keyfile ./privkey.pem
 
